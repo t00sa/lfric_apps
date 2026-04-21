831420cc885891d61449d9e20200723866d19dd3
iboutle <135141261+iboutle@users.noreply.github.com>
2026-04-21 13:52:00 +0100

    Revise and optimise PMSL calculation (#348)

--- applications/lfric2lfric/rose-meta/lfric-lfric2lfric/versions.py
+++ applications/lfric2lfric/rose-meta/lfric-lfric2lfric/versions.py
@@ -78,3 +78,19 @@ class vn31_t363(MacroUpgrade):
         self.add_setting(config, ["namelist:physics", "lw_segment_limit"], "32")
 
         return config, self.reports
+
+
+class vn31_t348(MacroUpgrade):
+    """Upgrade macro for ticket #348 by Ian Boutle."""
+
+    BEFORE_TAG = "vn3.1_t363"
+    AFTER_TAG = "vn3.1_t348"
+
+    def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-gungho
+        # Use PMSL halo calculations by default
+        self.add_setting(
+            config, ["namelist:physics", "pmsl_halo_calcs"], ".true."
+        )
+
+        return config, self.reports
--- science/gungho/rose-meta/lfric-gungho/versions.py
+++ science/gungho/rose-meta/lfric-gungho/versions.py
@@ -54,5 +54,20 @@ class vn31_t363(MacroUpgrade):
         """Set segmentation size limit for short and long wave radiation kernels"""
         self.add_setting(config, ["namelist:physics", "sw_segment_limit"], "32")
         self.add_setting(config, ["namelist:physics", "lw_segment_limit"], "32")
+        return config, self.reports
+
+
+class vn31_t348(MacroUpgrade):
+    """Upgrade macro for ticket #348 by Ian Boutle."""
+
+    BEFORE_TAG = "vn3.1_t363"
+    AFTER_TAG = "vn3.1_t348"
+
+    def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-gungho
+        # Use PMSL halo calculations by default
+        self.add_setting(
+            config, ["namelist:physics", "pmsl_halo_calcs"], ".true."
+        )
 
         return config, self.reports
